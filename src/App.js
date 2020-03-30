29fe84323e5c1026a78334846d667e96d2389da8
Conor Howland <conor.howland@outlook.com>
2020-03-30 23:00:32 +0000

    Add full functionality to sidebar.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class SteeMessages extends React.Component {
     super(props);
 
     // State - for holding values from inputs and other HTML elements
-    this.state = {'pmAddUsername': 'null', 'showAddChannels': false};
+    this.state = {'pmAddUsername': 'null', 'showAddChannels': false, 'channelAddName': '', 'showRemoveChannels': false, 'channelListUpdate': 0};
 
     // Check if channels are stored in browser
     if (store.get('channels')) {
@@ -32,7 +32,7 @@ class SteeMessages extends React.Component {
   
   // Show remove channels overlay, allowing users to click to delete channels.
   toggleRemoveChannels() {
-
+    this.setState({showRemoveChannels: !this.state.showRemoveChannels});
   }
 
   // Add a channel to a user's channel list
@@ -50,19 +50,20 @@ class SteeMessages extends React.Component {
   }
 
   // Remove a channel from a user's channel list
-  removeChannel(name, isPrivateMessage=true) {
+  removeChannel (name, isPrivateMessage=true) {
     if (isPrivateMessage) {
       for( var i = 0; i < this.channel_list.private.length; i++){ if ( this.channel_list.private[i] === name) { this.channel_list.private.splice(i, 1); i--; }}
     } else {
       for( var z = 0; z < this.channel_list.group.length; z++){ if ( this.channel_list.group[z]["name"] === name) { this.channel_list.group.splice(z, 1); i--; }}
     }
+    this.setState({channelListUpdate: this.state.channelListUpdate + 1});
     store.set('channels', this.channel_list);
   }
 
   // Pass required functions down to ChannelList, and return the result for use in the app
   renderChannelList() {
     return (
-      <ChannelList add_channels={() => this.toggleAddChannels()} remove_channels={() => this.toggleRemoveChannels()} channels={this.channel_list} />
+      <ChannelList showRemoveIcon={this.state.showRemoveChannels} addChannelsDialog={() => this.toggleAddChannels()} removeChannelsDialog={() => this.toggleRemoveChannels()} removeChannel={(name, isPrivateMessage) => this.removeChannel(name, isPrivateMessage)} channels={this.channel_list} />
     );
   }
 
@@ -74,12 +75,18 @@ class SteeMessages extends React.Component {
         <main>
           <div id="add-channels-overlay" className={"overlay" + (this.state.showAddChannels ? "" : " hidden")}>
             <div id="add-channels" className="sm-modal">
+              <button className="close-button" onClick={event => this.setState({showAddChannels: false})}>&times;</button>
               <h4>Add Channel</h4>
               <div className="row">
                 <div className="col col-offset-2">
                   <div>
                     <h5>Group Chat</h5>
                     <p>Start a chat in a public group.</p>
+                    <div className="input-with-icon">
+                      <div className="input-icon" id="person-avatar-dm" style={{backgroundImage: `url( "https://steemitimages.com/u/dmessages/avatar")`}} alt="null"></div>
+                      <input type="text" id="private_message_username" onChange={event => this.setState({channelAddName: event.target.value.toLowerCase()})} maxLength="16" />
+                      <button className="input-action" onClick={event => {if(this.state.channelAddName !== "") {this.addChannel(this.state.channelAddName,false,"dmessages"); this.setState({showAddChannels: false})}}}>+</button>
+                    </div>
                   </div>
                 </div>
                 <div className="col col-offset-2">
@@ -89,6 +96,7 @@ class SteeMessages extends React.Component {
                     <div className="input-with-icon">
                       <div className="input-icon" id="person-avatar-dm" style={{backgroundImage: `url( "https://steemitimages.com/u/${this.state.pmAddUsername}/avatar")`}} alt="null"></div>
                       <input type="text" id="private_message_username" onChange={event => this.setState({pmAddUsername: event.target.value.toLowerCase()})} maxLength="16" />
+                      <button className="input-action" onClick={event => {if(this.state.pmAddUsername !== "" && this.state.pmAddUsername !== "null") {this.addChannel(this.state.pmAddUsername); this.setState({showAddChannels: false})}}}>+</button>
                     </div>
                   </div>
                 </div>
@@ -118,12 +126,12 @@ function ChannelList(props) {
 
   return (
     <div id="channel-bar">
-      {props.channels.private.map((item) => (<ChannelIcon key={item} user={item}/>))}
+      {props.channels.private.map((item) => (<ChannelIcon key={item} showRemoveIcon={props.showRemoveIcon} removeChannel={()=>props.removeChannel(item)} user={item}/>))}
       {first_break}
-      {props.channels.group.map((item) => (<ChannelIcon key={item.name} name={item.name} avatar={item.avatar}/>))}
+      {props.channels.group.map((item) => (<ChannelIcon key={item.name} name={item.name} removeChannel={()=>props.removeChannel(item.name, false)} showRemoveIcon={props.showRemoveIcon} avatar={item.avatar}/>))}
       {second_break}
-      <div className="channel-icon" onClick={props.add_channels} data-tip="Add">+</div>
-      <div className="channel-icon" onClick={props.remove_channels} data-tip="Remove"><em className="fas fa-wrench"></em></div>
+      <div className="command-icon" onClick={props.addChannelsDialog} data-tip="Add">+</div>
+      <div className="command-icon" onClick={props.removeChannelsDialog} data-tip="Remove">-</div>
     </div>
   );
 }
@@ -139,7 +147,8 @@ function ChannelIcon(props) {
     avatar_name = props.avatar;
   }
   return (
-    <div className="channel-icon show-delete" style={{backgroundImage: `url( "https://steemitimages.com/u/${avatar_name}/avatar")`}} alt={channel_name} data-tip={channel_name}><div className="delete-channel">&times;</div></div>
+    <div className={"channel-icon" + (props.showRemoveIcon ? " show-delete" : "")}
+    style={{backgroundImage: `url( "https://steemitimages.com/u/${avatar_name}/avatar")`}}  alt={channel_name} data-tip={channel_name}><div onClick={props.removeChannel} className="delete-channel">&times;</div></div>
   );
 }
 

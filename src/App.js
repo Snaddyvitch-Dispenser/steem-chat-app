f99273f1f9c181bcb12a4a04e91c5028f9c2a53c
Conor <conor.howland@outlook.com>
2020-04-16 21:36:39 +0000

    Adding login via Hive Keychain

--- src/App.js
+++ src/App.js
@@ -9,25 +9,18 @@ class SteeMessages extends React.Component {
     super(props);
 
     // State - for holding values from inputs and other HTML elements
-    this.state = {'pmAddUsername': 'null', 'showAddChannels': false, 'channelAddName': '', 'showRemoveChannels': false, 'channelListUpdate': 0};
+    this.state = {'pmAddUsername': '', 'showAddChannels': false, 'channelAddName': '', 'showRemoveChannels': false, 'channelList': {"group": [], "private": []}};
 
     // Check if channels are stored in browser
     if (store.get('channels')) {
       // Load channel list from browser
-      this.channel_list = store.get('channels');
-
-      // Remove any empty named private message channels (bug due to localstorage used to store channels)
-      this.removeChannel("");
-    } else {
-      // Start new empty channel list
-      this.channel_list = {"group": [], "private": []};
+      this.state.channelList = store.get('channels');
     }
   }
 
   // Toggle add channels box
   toggleAddChannels() {
-    console.log("add channels toggled.");
-    this.setState({'showAddChannels': !this.state.showAddChannels});
+    this.setState({showAddChannels: !this.state.showAddChannels});
   }
   
   // Show remove channels overlay, allowing users to click to delete channels.
@@ -37,103 +30,186 @@ class SteeMessages extends React.Component {
 
   // Add a channel to a user's channel list
   addChannel(name, isPrivateMessage=true, avatar="") {
+    var channels = this.state.channelList;
     if (isPrivateMessage) {
-      if ((this.channel_list.private.indexOf(name) === -1)) {
-        this.channel_list.private.push(name);
+      if ((channels.private.indexOf(name) === -1)) {
+        channels.private.push(name);
       }
     } else {
-      if ((this.channel_list.group.filter(value=> value["name"] === name).length === 0)) {
-        this.channel_list.group.push({"name": name, "avatar": avatar});
+      if ((channels.group.filter(value=> value["name"] === name).length === 0)) {
+        channels.group.push({"name": name, "avatar": avatar});
       }
     }
-    store.set('channels', this.channel_list);
+    this.setState({channelList: channels});
+    store.set('channels', this.state.channelList);
   }
 
   // Remove a channel from a user's channel list
   removeChannel (name, isPrivateMessage=true) {
+    var channels = this.state.channelList;
     if (isPrivateMessage) {
-      for( var i = 0; i < this.channel_list.private.length; i++){ if ( this.channel_list.private[i] === name) { this.channel_list.private.splice(i, 1); i--; }}
+      // Loop through all Private Messages
+      for( var pmsChecked = 0; pmsChecked < this.state.channelList.private.length; pmsChecked++) {
+        // If Private Message username matches
+        if (channels.private[pmsChecked] === name) { 
+            channels.private.splice(pmsChecked, 1); // Remove elements
+            pmsChecked--; // Decrement by one, because an element has been removed
+          }
+        }
     } else {
-      for( var z = 0; z < this.channel_list.group.length; z++){ if ( this.channel_list.group[z]["name"] === name) { this.channel_list.group.splice(z, 1); i--; }}
+      // Loop through all channels
+      for( var channelsChecked = 0; channelsChecked < this.state.channelList.group.length; channelsChecked++){
+        // If channel matches
+        if ( this.state.channelList.group[channelsChecked]["name"] === name) {
+          this.state.channelList.group.splice(channelsChecked, 1); // Remove element
+          channelsChecked--; // Decrement by one, because we just removed an element. 
+      }}
     }
-    this.setState({channelListUpdate: this.state.channelListUpdate + 1});
-    store.set('channels', this.channel_list);
+    // Update browser copy & state
+    this.setState({channelList: channels});
+    store.set('channels', this.state.channelList);
   }
 
   // Pass required functions down to ChannelList, and return the result for use in the app
   renderChannelList() {
     return (
-      <ChannelList showRemoveIcon={this.state.showRemoveChannels} addChannelsDialog={() => this.toggleAddChannels()} removeChannelsDialog={() => this.toggleRemoveChannels()} removeChannel={(name, isPrivateMessage) => this.removeChannel(name, isPrivateMessage)} channels={this.channel_list} />
+      <ChannelList showRemoveIcon={this.state.showRemoveChannels} addChannelsDialog={() => this.toggleAddChannels()} removeChannelsDialog={() => this.toggleRemoveChannels()} removeChannel={(name, isPrivateMessage) => this.removeChannel(name, isPrivateMessage)} channels={this.state.channelList} />
     );
   }
 
+  renderAddChannelDialog(){
+    if (this.state.showAddChannels) {
+      return (
+        <div id="add-channels-overlay" className="overlay">
+          <div id="add-channels" className="sm-modal">
+            <button className="close-button" onClick={event => this.setState({showAddChannels: false})}>&times;</button>
+            <h4>Add Channel</h4>
+            <div className="row">
+              <div className="col col-offset-2">
+                <div>
+                  <h5>Group Chat</h5>
+                  <p>Start a chat in a public group.</p>
+                  <div className="input-with-icon">
+                    <div className="input-icon" id="person-avatar-dm" style={{backgroundImage: `url( "https://steemitimages.com/u/dmessages/avatar")`}} alt="null"></div>
+                    <input type="text" id="private_message_username" value={this.state.channelAddName} onChange={event => this.setState({channelAddName: event.target.value.toLowerCase()})} maxLength="16" />
+                    <button className="input-action" onClick={event => {if(this.state.channelAddName !== "") {this.addChannel(this.state.channelAddName,false,"dmessages"); this.setState({showAddChannels: false, channelAddName: ""})}}}>+</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-offset-2">
+                <div>
+                  <h5>Private Message</h5>
+                  <p>Send a private message to another user.</p>
+                  <div className="input-with-icon">
+                    <div className="input-icon" id="person-avatar-dm" style={{backgroundImage: `url( "https://steemitimages.com/u/${this.state.pmAddUsername}/avatar")`}} alt="null"></div>
+                    <input type="text" value={this.state.pmAddUsername} id="private_message_username" onChange={event => this.setState({pmAddUsername: event.target.value.toLowerCase()})} maxLength="16" />
+                    <button className="input-action" onClick={event => {if(this.state.pmAddUsername !== "" && this.state.pmAddUsername !== "null") {this.addChannel(this.state.pmAddUsername); this.setState({showAddChannels: false, pmAddUsername: ""})}}}>+</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return "";
+    }
+  }
 
   // Render the entire app
   render() {
     return (
       <div className="App">
         <main>
-          <div id="add-channels-overlay" className={"overlay" + (this.state.showAddChannels ? "" : " hidden")}>
-            <div id="add-channels" className="sm-modal">
-              <button className="close-button" onClick={event => this.setState({showAddChannels: false})}>&times;</button>
-              <h4>Add Channel</h4>
-              <div className="row">
-                <div className="col col-offset-2">
-                  <div>
-                    <h5>Group Chat</h5>
-                    <p>Start a chat in a public group.</p>
-                    <div className="input-with-icon">
-                      <div className="input-icon" id="person-avatar-dm" style={{backgroundImage: `url( "https://steemitimages.com/u/dmessages/avatar")`}} alt="null"></div>
-                      <input type="text" id="private_message_username" onChange={event => this.setState({channelAddName: event.target.value.toLowerCase()})} maxLength="16" />
-                      <button className="input-action" onClick={event => {if(this.state.channelAddName !== "") {this.addChannel(this.state.channelAddName,false,"dmessages"); this.setState({showAddChannels: false})}}}>+</button>
-                    </div>
-                  </div>
-                </div>
-                <div className="col col-offset-2">
-                  <div>
-                    <h5>Private Message</h5>
-                    <p>Send a private message to another user.</p>
-                    <div className="input-with-icon">
-                      <div className="input-icon" id="person-avatar-dm" style={{backgroundImage: `url( "https://steemitimages.com/u/${this.state.pmAddUsername}/avatar")`}} alt="null"></div>
-                      <input type="text" id="private_message_username" onChange={event => this.setState({pmAddUsername: event.target.value.toLowerCase()})} maxLength="16" />
-                      <button className="input-action" onClick={event => {if(this.state.pmAddUsername !== "" && this.state.pmAddUsername !== "null") {this.addChannel(this.state.pmAddUsername); this.setState({showAddChannels: false})}}}>+</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <LoginPanel/>
+          {this.renderAddChannelDialog()}
           {this.renderChannelList()}
+          <Channel />
         </main>
-        <ReactTooltip effect="solid" place="right" backgroundColor="#000" />
+        <script src="https://kit.fontawesome.com/e169a3044d.js" crossorigin="anonymous"></script>
       </div>
     );
   }
 }
 
+class Channel extends React.Component {
+  render() {
+    return (
+      <div className="channel">
+        <div className="channel-info">
+          <p><em className="fas fa-at"></em> CADawg</p>
+        </div>
+        <div className="channel-content">
 
+        </div>
+        <div className="channel-send">
+          <input type="text" className="message-text" />
+          <button className="message-send-button"><em className="far fa-paper-plane"></em></button>
+        </div>
+      </div>
+    );
+  }
+}
+
+class LoginPanel extends React.Component {
+  getLoginFormContent() {
+    if (window.hive_keychain === null) {
+
+    } else {
+      return (
+        <div>
+          <p>Sorry, but to use this app, you need to install Hive Keychain</p>
+          <p>Get it for
+            <a href="https://addons.mozilla.org/en-US/firefox/addon/hive-keychain">Firefox</a>
+          </p>
+          <img src="https://camo.githubusercontent.com/468645d9ab6ea045a344b22d963c592ebe5ee511/687474703a2f2f752e6375626575706c6f61642e636f6d2f617263616e67652f794f644935672e706e67" alt="" />
+        </div>
+      );
+    }
+  }
+
+  render () {
+    return (
+      <div className="login-panel">
+        <div className="login-form">
+          {this.getLoginFormContent()}
+        </div>
+      </div>
+    );
+  }
+}
 
 // List of channels (sidebar) containing <ChannelIcon />s
-function ChannelList(props) {
-  let first_break = "";
-  if (props.channels.private.length > 0) {
-    first_break = <div className="hr"></div>;
-  }
-  let second_break = "";
-  if (props.channels.group.length > 0) {
-    second_break = <div className="hr"></div>;
+class ChannelList extends React.Component {
+  // Update tooltips (for added channels) when the list changes
+  componentDidUpdate() {
+    ReactTooltip.rebuild();
   }
 
-  return (
-    <div id="channel-bar">
-      {props.channels.private.map((item) => (<ChannelIcon key={item} showRemoveIcon={props.showRemoveIcon} removeChannel={()=>props.removeChannel(item)} user={item}/>))}
-      {first_break}
-      {props.channels.group.map((item) => (<ChannelIcon key={item.name} name={item.name} removeChannel={()=>props.removeChannel(item.name, false)} showRemoveIcon={props.showRemoveIcon} avatar={item.avatar}/>))}
-      {second_break}
-      <div className="command-icon" onClick={props.addChannelsDialog} data-tip="Add">+</div>
-      <div className="command-icon" onClick={props.removeChannelsDialog} data-tip="Remove">-</div>
-    </div>
-  );
+  // Render the channel list
+  render() {
+    // Work out if separators are needed
+    let first_break = "";
+    if (this.props.channels.private.length > 0) {
+      first_break = <div className="hr"></div>;
+    }
+    let second_break = "";
+    if (this.props.channels.group.length > 0) {
+      second_break = <div className="hr"></div>;
+    }
+
+    return (
+      <div id="channel-bar">
+        {this.props.channels.private.map((item) => (<ChannelIcon key={item} showRemoveIcon={this.props.showRemoveIcon} removeChannel={()=>this.props.removeChannel(item)} user={item}/>))}
+        {first_break}
+        {this.props.channels.group.map((item) => (<ChannelIcon key={item.name} name={item.name} removeChannel={()=>this.props.removeChannel(item.name, false)} showRemoveIcon={this.props.showRemoveIcon} avatar={item.avatar}/>))}
+        {second_break}
+        <div className="command-icon" onClick={this.props.addChannelsDialog} data-tip="Add">+</div>
+        <div className="command-icon" onClick={this.props.removeChannelsDialog} data-tip="Remove">-</div>
+        <ReactTooltip effect="solid" place="right" backgroundColor="#000" />
+      </div>
+    );
+  }
 }
 
 function ChannelIcon(props) {
@@ -148,7 +224,7 @@ function ChannelIcon(props) {
   }
   return (
     <div className={"channel-icon" + (props.showRemoveIcon ? " show-delete" : "")}
-    style={{backgroundImage: `url( "https://steemitimages.com/u/${avatar_name}/avatar")`}}  alt={channel_name} data-tip={channel_name}><div onClick={props.removeChannel} className="delete-channel">&times;</div></div>
+    style={{backgroundImage: `url( "https://images.hive.blog/u/${avatar_name}/avatar")`}} alt={channel_name} data-tip={channel_name}><div onClick={props.removeChannel} className="delete-channel">&times;</div></div>
   );
 }
 

ffd1e45ccb4b7f07373b54f3d950eb80cfbbd9c8
Conor <conor.howland@outlook.com>
2020-05-03 16:17:51 +0000

    Websocket Connected for Chats

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactTooltip from 'react-tooltip'
 import './App.scss';
-const firefox_logo = require('./firefox.svg');
-const chrome_logo = require('./chrome.svg');
+import { ToastContainer, toast } from 'react-toastify'
+import firefox_logo from './firefox.svg'
+import chrome_logo from './chrome.svg'
+import 'react-toastify/dist/ReactToastify.css';
+import Websocket from 'react-websocket';
 const store = require('store/dist/store.modern');
 const axios = require("axios");
 
@@ -15,12 +18,23 @@ class SteeMessages extends React.Component {
     super(props);
 
     // State - for holding values from inputs and other HTML elements
-    this.state = {'pmAddUsername': '', 'showAddChannels': false, 'channelAddName': '', 'showRemoveChannels': false, 'channelList': {"group": [], "private": []}};
+    this.state = {'pmAddUsername': '', 'showAddChannels': false, 'channelAddName': '', 'showRemoveChannels': false, 'channelList': {"group": [], "private": []}, 'currentUser': '', "currentChannel": {"name": "", "isPrivateMessage": true}};
 
     // Check if channels are stored in browser
     if (store.get('channels')) {
       // Load channel list from browser
       this.state.channelList = store.get('channels');
+    }
+
+    // Load user from storage if saved
+    if (store.get('user')) {
+      if (store.get('user') !== "") {
+        this.state.currentUser = store.get('user');
+      }
+    }
+
+    if (store.get('currentChannel')) {
+      this.state.currentChannel = store.get('currentChannel');
     }
   }
 
@@ -76,10 +90,15 @@ class SteeMessages extends React.Component {
     store.set('channels', this.state.channelList);
   }
 
+  onChannelClick(channel, isPrivateMessage=true){
+    this.setState({currentChannel: {"name": channel, "isPrivateMessage": isPrivateMessage}});
+    store.set('currentChannel', this.state.currentChannel);
+  }
+
   // Pass required functions down to ChannelList, and return the result for use in the app
   renderChannelList() {
     return (
-      <ChannelList showRemoveIcon={this.state.showRemoveChannels} addChannelsDialog={() => this.toggleAddChannels()} removeChannelsDialog={() => this.toggleRemoveChannels()} removeChannel={(name, isPrivateMessage) => this.removeChannel(name, isPrivateMessage)} channels={this.state.channelList} />
+      <ChannelList onChannelClick={(channel, isPrivateMessage=true) => this.onChannelClick(channel, isPrivateMessage)} showRemoveIcon={this.state.showRemoveChannels} addChannelsDialog={() => this.toggleAddChannels()} removeChannelsDialog={() => this.toggleRemoveChannels()} removeChannel={(name, isPrivateMessage) => this.removeChannel(name, isPrivateMessage)} channels={this.state.channelList} />
     );
   }
 
@@ -107,8 +126,8 @@ class SteeMessages extends React.Component {
                   <h5>Private Message</h5>
                   <p>Send a private message to another user.</p>
                   <div className="input-with-icon">
-                    <div className="input-icon" id="person-avatar-dm" style={{backgroundImage: `url( "https://steemitimages.com/u/${this.state.pmAddUsername}/avatar")`}} alt="null"></div>
-                    <input type="text" value={this.state.pmAddUsername} id="private_message_username" onChange={event => this.setState({pmAddUsername: event.target.value.toLowerCase()})} maxLength="16" />
+                    <div maxLength="100" className="input-icon" id="person-avatar-dm" style={{backgroundImage: `url( "https://steemitimages.com/u/${this.state.pmAddUsername}/avatar")`}} alt="null"></div>
+                    <input type="text" value={this.state.pmAddUsername} id="private_message_username" onChange={event => this.setState({pmAddUsername: event.target.value.toLowerCase()})} maxLength="32" />
                     <button className="input-action" onClick={event => {if(this.state.pmAddUsername !== "" && this.state.pmAddUsername !== "null") {this.addChannel(this.state.pmAddUsername); this.setState({showAddChannels: false, pmAddUsername: ""})}}}>+</button>
                   </div>
                 </div>
@@ -122,15 +141,34 @@ class SteeMessages extends React.Component {
     }
   }
 
+  userSignedIn(user) {
+    this.setState({"currentUser": user});
+    store.set('user', this.state.currentUser);
+  }
+
+  renderLoginPanel() {
+    return (<LoginPanel signedInAs={this.state.currentUser} onSignIn={(user) => {this.userSignedIn(user)}} />);
+  }
+
+  renderChannel() {
+    return (<Channel userSignedIn={this.state.currentUser} currentChannel={this.state.currentChannel} />);
+  }
+
+  websocketRecieveMessage(data) {
+
+  }
+
   // Render the entire app
   render() {
     return (
       <div className="App">
+        <ToastContainer />
         <main>
-          <LoginPanel signedInAs="" />
+          <Websocket url="ws://localhost" onMessage={this.websocketRecieveMessage.bind(this)} onClose={() => toast.error("Disconnected, retrying.")} onOpen={() => toast.success("Connected!")} />
+          {this.renderLoginPanel()}
           {this.renderAddChannelDialog()}
           {this.renderChannelList()}
-          <Channel />
+          {this.renderChannel()}
         </main>
         <script src="https://kit.fontawesome.com/e169a3044d.js" crossorigin="anonymous"></script>
       </div>
@@ -143,14 +181,18 @@ class Channel extends React.Component {
     return (
       <div className="channel">
         <div className="channel-info">
-          <p><em className="fas fa-at"></em> CADawg</p>
+          <p><em className={"fas fa-" + (this.props.currentChannel.isPrivateMessage ? "at" : "hashtag")}></em> {this.props.currentChannel.name}</p>
         </div>
         <div className="channel-content">
 
         </div>
-        <div className="channel-send">
-          <input type="text" className="message-text" />
-          <button className="message-send-button"><em className="far fa-paper-plane"></em></button>
+        <div className="channel-send row">
+          <input type="text" className="message-text col-sm-11" />
+          <div className="channel-buttons col-sm-1">
+            <button className="message-button message-send-button btn"><em className="far fa-paper-plane"></em></button>
+            <button className="message-button message-add-button btn">+</button>
+            <button className="message-button message-user-button btn" style={{backgroundImage: `url( "https://images.hive.blog/u/${this.props.userSignedIn}/avatar")`}}></button>
+          </div>
         </div>
       </div>
     );
@@ -162,38 +204,66 @@ class LoginPanel extends React.Component {
     super(props);
 
     // State - for holding values from inputs and other HTML elements
-    this.state = {'loginAs': ''};
+    this.state = {'loginAs': '', 'keychain': true};
   }
   
   startLoginChallenge(){
     // If user text not blank
-    if (this.state.loginAs !== "") {
+    if (this.state.loginAs !== "") {      
       // Get challenge
       axios.get("https://api.dmessages.app/challenge.php", { params: { user: this.state.loginAs }}).then((response) => {
         if (response.data.success) {
-          console.log(response.data);
-          window.hive_keychain.requestVerifyKey(response.data.user, response.data.challenge, "Memo", (data) => {
-	console.log(data);
-});
+          window.hive_keychain.requestVerifyKey(response.data.data.user,response.data.data.challenge, "Memo", (answer) => {
+            if (answer.success) {
+              let requestData = new FormData();
+              requestData.set('user', response.data.data.user);
+              requestData.set('answer', answer.result);
+              axios({
+                method: 'post',
+                url: 'https://api.dmessages.app/challenge_solve.php',
+                data: requestData
+              }).then((response) => {
+                if (response.data.success) {
+                  toast.success("You're signed in!")
+                  this.props.onSignIn(response.data.user);
+                } else {
+                  if (response.data.user === undefined) {
+                    toast.error(response.data.error_message);
+                  } else {
+                    toast.success("You're signed in!")
+                    this.props.onSignIn(response.data.user);
+                  }
+                }
+              });
+            } else {
+              toast.error(answer.message);
+            }
+          });
         } else {
-          console.log(response.data.error_message);
+          if (response.data.user === undefined) {
+            toast.error(response.data.error_message);
+          } else {
+            toast.success("You're signed in!");
+            this.props.onSignIn(response.data.user);
+          }
         }
       });
     } else {
-
+      toast.error("Please enter a username");
     }
   }
 
   getLoginFormContent() {
-    if (window.hive_keychain !== null || window.hive_keychain !== undefined) {
-      return (
-        <span className="login-info">
-          <h5>Enter your username to login</h5>
-          <input onChange={event => this.setState({loginAs: event.target.value.toLowerCase()})}  type="text" id="username" />
-          <button onClick={event => this.startLoginChallenge()}>Login!</button>
-        </span>
-      );
-    } else {
+    setTimeout(() => {
+      try {
+        window.hive_keychain.requestHandshake(() => {
+          this.setState({keychain: true});
+        });
+      } catch {
+        this.setState({keychain: false});
+      } // Catch in case Keychain isn't loaded/installed yet
+    }, 500);
+    if ((window.hive_keychain === null || window.hive_keychain === undefined) && this.state.keychain === false) {
       return (
         <span className="login-info">
           <h3>This app requires Hive Keychain</h3>
@@ -206,8 +276,19 @@ class LoginPanel extends React.Component {
               <img src={chrome_logo} alt="Chrome" />
             </a>
           </div>
-          <p class="copyright-text">The Firefox logo is a trademark of the Mozilla Foundation in the U.S. and other countries. Google Chrome is a trademark of Google LLC</p>
+          <p className="copyright-text">The Firefox logo is a trademark of the Mozilla Foundation in the U.S. and other countries. Google Chrome is a trademark of Google LLC</p>
           <p></p>
+        </span>
+      );
+    } else {
+      return (
+        <span className="login-info">
+          <h5>Enter your username to login with Hive Keychain</h5>
+          <div className="form-row">
+            <input className="form-control col-sm-9" onChange={event => this.setState({loginAs: event.target.value.toLowerCase()})}  type="text" id="username" />
+            <div className="col-sm-1"></div>
+            <button className="btn btn-primary col-sm-2" onClick={event => this.startLoginChallenge()}>Login!</button>
+          </div>
         </span>
       );
     }
@@ -249,9 +330,9 @@ class ChannelList extends React.Component {
 
     return (
       <div id="channel-bar">
-        {this.props.channels.private.map((item) => (<ChannelIcon key={item} showRemoveIcon={this.props.showRemoveIcon} removeChannel={()=>this.props.removeChannel(item)} user={item}/>))}
+        {this.props.channels.private.map((item) => (<ChannelIcon key={item} showRemoveIcon={this.props.showRemoveIcon} removeChannel={()=>this.props.removeChannel(item)} onClick={() => this.props.onChannelClick(item)} user={item}/>))}
         {first_break}
-        {this.props.channels.group.map((item) => (<ChannelIcon key={item.name} name={item.name} removeChannel={()=>this.props.removeChannel(item.name, false)} showRemoveIcon={this.props.showRemoveIcon} avatar={item.avatar}/>))}
+        {this.props.channels.group.map((item) => (<ChannelIcon key={item.name} name={item.name} onClick={() => this.props.onChannelClick(item.name, false)} removeChannel={()=>this.props.removeChannel(item.name, false)} showRemoveIcon={this.props.showRemoveIcon} avatar={item.avatar}/>))}
         {second_break}
         <div className="command-icon" onClick={this.props.addChannelsDialog} data-tip="Add">+</div>
         <div className="command-icon" onClick={this.props.removeChannelsDialog} data-tip="Remove">-</div>
@@ -272,7 +353,7 @@ function ChannelIcon(props) {
     avatar_name = props.avatar;
   }
   return (
-    <div className={"channel-icon" + (props.showRemoveIcon ? " show-delete" : "")}
+    <div onClick={props.onClick} className={"channel-icon" + (props.showRemoveIcon ? " show-delete" : "")}
     style={{backgroundImage: `url( "https://images.hive.blog/u/${avatar_name}/avatar")`}} alt={channel_name} data-tip={channel_name}><div onClick={props.removeChannel} className="delete-channel">&times;</div></div>
   );
 }
